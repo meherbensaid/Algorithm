5af34234c6a95d6db897216de0890fa3d147c390
maher <meherbensaid012@gmail.com>
2018-02-15 17:58:44 +0100

    Linked List Test Bug Fixed

---
+++ Algorithm/LinkedList/LinkedList.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+namespace Algorithm.LinkedList
+{
+    public class LinkedList
+    {
+        public Node Head;
+
+        public Node Current;
+
+        public int count;
+
+        public LinkedList()
+        {
+            count = 0;
+        }
+        public LinkedList(int headValue)
+        {
+            Head = new Node() { Value = headValue };
+            Current = Head;
+            count = 1;
+        }
+
+        public LinkedList(int[] values) : this(values[0])
+        {
+           
+            for (int i = 1; i < values.Length; i++)
+            {
+                this.AddAtLast(values[i]);
+            }
+
+            this.Current = Head;
+
+        }
+
+        public void AddAtLast(int value)
+        {
+            Node newNode = new Node() { Value = value };
+            while (Current.Next != null)
+            {
+                Current = Current.Next;
+            }
+            Current.Next = newNode;
+            Current = newNode;
+            count++;
+        }
+        public void AddAtStart(int value)
+        {
+            var newNode = new Node
+            {
+                Value = value,
+                Next = Head
+            };
+            Head = newNode;
+            count++;
+        }
+
+        public void AddAtPosition(int value, int position)
+        {
+            if (position == 0)
+            {
+                AddAtStart(value);
+                return;
+            }
+
+            if (position >= count) { AddAtLast(value); return; }
+            int i = 0;
+            Current = Head;
+            while (i != position - 1)
+            {
+                Current = Current.Next;
+                i++;
+            }
+
+            var newNode = new Node
+            {
+                Value = value,
+                Next = Current.Next
+            };
+            Current.Next = newNode;
+            Current = newNode;
+            count++;
+        }
+
+        public Node GetElementByValue(int value)
+        {
+            if (this.count == 0) return null;
+
+            Current = Head;
+            while (Current.Next != null)
+            {
+                Current = Current.Next;
+                if (Current.Value == value) return Current;
+            }
+
+                return null;
+        }
+
+        public Node GetElementAt(int index)
+        {
+            Current = Head;
+            var i = 0;
+            if (this.count == 0) return null;
+            if (index > this.count - 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index),"The index should be greater than the size of the linkedList");
+            }
+            while (Current.Next != null && i!= index)
+            {
+                Current = Current.Next;
+                i++;
+            }
+
+            return Current;
+        }
+
+        public Node DeleteAtFirst()
+        {
+            if (count == 1)
+            {
+                Head = null;
+                count--;
+                return null;
+            }
+            else
+            {
+                var removedNode = Head;
+                Head = Head.Next;
+                Current = Head;
+                count--;
+                return removedNode;
+            }
+        }
+        public Node DeleteAtLast()
+        {
+            var tmpNode = Head;
+            while (tmpNode.Next.Next != null)
+            {
+                tmpNode = tmpNode.Next;
+            }
+
+            var removedNode = tmpNode.Next;
+            tmpNode.Next = null;
+            Current = tmpNode;
+            count--;
+            return removedNode;
+        }
+
+        public Node DeleteAtPosition(int position)
+        {
+            if (position == 0) return DeleteAtFirst();
+            if (position == count-1) { return DeleteAtLast();  }
+            var i = 0;
+            Current = Head;
+            while (i != position )
+            {
+                Current = Current.Next;
+                i++;
+            }
+
+            var removedNode = Current;
+            Current = Current.Next;
+            count--;
+            return removedNode;
+        }
+        public string DisplayNode()
+        {
+            var displayNode = "";
+            var tmpNode = Head;
+            while (tmpNode != null)
+            {
+                displayNode+=$"{tmpNode.Value},";
+                tmpNode = tmpNode.Next;
+            }
+
+           return displayNode.Substring(0, displayNode.Length-1);
+
+        }
+
+    }
+}
---
+++ AlgorithmTest/LinkedListTest.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm.LinkedList;
+using NUnit.Framework;
+
+namespace AlgorithmTest
+{
+    [TestFixture]
+    class LinkedListTest
+    {
+        private LinkedList _linkedList;
+        private LinkedList _emptyLinkedList;
+        private LinkedList _linkedListFull;
+
+       [SetUp]
+        public void SetUp()
+        {
+            _linkedList=new LinkedList(1);
+            _emptyLinkedList=new LinkedList();
+
+            var tab = new int[] { 1, 2, 3 };
+            _linkedListFull = new LinkedList(tab);
+        }
+
+        [Test]
+        public void Should_Add_Element_At_Last_with_List_that_Have_One_elemnt()
+        {
+            var value = 12;
+
+            var expected = 12;
+
+            _linkedList.AddAtLast(12);
+            var actualValue = _linkedList.Current.Value;
+            var nexNode = _linkedList.Current.Next;
+
+            Assert.AreEqual(expected,actualValue);
+            Assert.AreEqual(null,nexNode);
+        }
+        [Test]
+        public void Should_Add_Element_At_Last_with_List_that_Have_Many_elemnt()
+        {
+            var tab = new int[] {1, 2, 3};
+            _linkedList=new LinkedList(tab);
+
+            var value = 12;
+
+            var expected = 12;
+
+            _linkedList.AddAtLast(value);
+            var actualValue = _linkedList.Current.Value;
+            var nexNode = _linkedList.Current.Next;
+
+            Assert.AreEqual(expected, actualValue);
+            Assert.AreEqual(null, nexNode);
+        }
+
+        [Test]
+        public void Should_Add_Element_At_The_First_Position()
+        {
+            const int value = 14;
+            const int expected = 14;
+
+            _linkedList.AddAtStart(value);
+            var actual = _linkedList.Head.Value;
+
+            Assert.AreEqual(expected,actual);
+
+        }
+
+        [Test]
+        public void Should_Return_Element_When_We_Set_the_Element_Value()
+        {
+           
+            var value = 2;
+            var expected = 2;
+
+            var actual= _linkedListFull.GetElementByValue(value).Value;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Should_Return_Null_When_The_Element_Does_Not_Exist_In_The_Linked_List()
+        {
+          
+            const int value = 4;
+
+            var actual = _linkedListFull.GetElementByValue(value);
+
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void Should_Return_Null_When_The_List_Is_Empty_Get_By_Value()
+        {
+            var actual=_emptyLinkedList.GetElementByValue(12);
+
+            Assert.IsNull(actual);
+        }
+        [Test]
+        public void Should_Return_Null_When_The_List_Is_Empty_Get_By_position()
+        {
+            var actual = _emptyLinkedList.GetElementAt(12);
+
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void Should_Return_Element_At_The_index()
+        {
+            var index = 2;
+
+            var expected = 3;
+
+            var actual = _linkedListFull.GetElementAt(index).Value;
+
+            Assert.AreEqual(expected,actual);
+
+        }
+
+        [Test]
+        public void Should_Return_Null_When_The_List_Is_Empty_And_We_Set_Element_Index()
+        {
+            var actual = _emptyLinkedList.GetElementAt(12);
+
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void Should_Throw_Exception_When_Index_Is_Greater_Than_LinkedList_Size()
+        {
+            const int index = 120;
+
+            try
+            {
+                _linkedListFull.GetElementAt(index);
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Assert.IsTrue(e.Message.StartsWith("The index should be greater than the size of the linkedList"));
+            }
+        }
+        [Test]
+        public void Should_Add_Element_At_Position()
+        {
+            const int position = 2;
+            const int value = 17;
+            const int expected = 17;
+
+            _linkedListFull.AddAtPosition(value, position);
+            var actual = _linkedListFull.GetElementAt(position).Value;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Should_Add_Element_At_Position_0()
+        {
+            const int position = 0;
+            const int value = 17;
+            const int expected = 17;
+
+            _linkedListFull.AddAtPosition(value, position);
+            var actual = _linkedListFull.GetElementAt(position).Value;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Should_Add_Element_At_Last_Position()
+        {
+            const int position = 3;
+            const int value = 17;
+            const int expected = 17;
+
+            _linkedListFull.AddAtPosition(value, position);
+            var actual = _linkedListFull.GetElementAt(position).Value;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Should_Delete_At_First_Postion_In_Linked_List_Which_Contain_One_Element()
+        {
+            var expected= 0;
+
+            _linkedList.DeleteAtFirst();
+            var actual = _linkedList.count;
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void Should_Delete_At_First_Postion_In_Linked_List_Which_Contain_More_Than_One_Element()
+        {
+            var expected = _linkedListFull.Head.Next;
+
+            _linkedListFull.DeleteAtFirst();
+            var actual = _linkedListFull.Head;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Should_Delete_Last_Element()
+        {
+            var expected = 2;
+            var lastPoistion = _linkedListFull.count - 2;
+
+            _linkedListFull.DeleteAtLast();
+            var actual = _linkedListFull.GetElementAt(lastPoistion).Value;
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void Should_Delete_Element_At_Position()
+        {
+            const int position = 1;
+            var expected = 2;
+
+            var actual = _linkedListFull.DeleteAtPosition(position).Value;
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void Should_Delete_Element_At_Position_0()
+        {
+            const int position = 0;
+            var expected = 1;
+
+            var actual = _linkedListFull.DeleteAtPosition(position).Value;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Should_Delete_Element_At_Last_Position()
+        {
+            const int position =2 ;
+            var expected = 3;
+
+            var actual = _linkedListFull.DeleteAtPosition(position).Value;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Should_Dislay_LinkedList()
+        {
+            var expected = "1,2,3";
+
+            var actual = _linkedListFull.DisplayNode();
+
+            Assert.AreEqual(expected,actual);
+        }
+
+    }
+}
