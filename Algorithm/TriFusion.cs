8c32541a2a8e0c39b4691738d7c6c5387a51815a
maher <meherbensaid012@gmail.com>
2018-02-16 15:26:45 +0100

    Tri Fusion Test Bug Fixed

---
+++ Algorithm/TriFusion.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class TriFusion
+    {
+        /// <summary>
+        /// tri fusion
+        /// </summary>
+        public  void triFusion(int[] tab, int début, int fin)
+        {
+            int milieu;
+            if (début < fin)
+            {
+                milieu = (début + fin) / 2;
+                triFusion(tab, début, milieu);
+                triFusion(tab, milieu + 1, fin);
+                fusionner(tab, début, milieu, fin);
+            }
+        }
+
+        public  void fusionner(int[] tab, int début, int milieu, int fin)
+        {
+            int[] old_tab = (int[])tab.Clone();
+            // tab.clone est tres gourmand en temps d'execution surtout dans un algo recursif
+            // il faudrait passer par un tableau temporaire pour stocker les données triées.
+            // puis recopier la partie triée a la fin de la méthode.
+
+            int i1 = début; //indice dans la première moitié de old_tab
+            int i2 = milieu + 1; // indice dans la deuxième moitié de old_tab
+            int i = début; //indice dans le tableau tab
+
+            while (i1 <= milieu && i2 <= fin)
+            {
+                //quelle est la plus petite tête de liste?
+                if (old_tab[i1] <= old_tab[i2])
+                {
+                    tab[i] = old_tab[i1];
+                    i1++;
+                }
+                else
+                {
+                    tab[i] = old_tab[i2];
+                    i2++;
+                }
+                i++;
+             }
+
+            if (i <= fin)
+            {
+                while (i1 <= milieu)  // le reste de la première moitié
+                {
+                    tab[i] = old_tab[i1];
+                    i1++;
+                    i++;
+                }
+                while (i2 <= fin) // le reste de la deuxième moitié
+                {
+                    tab[i] = old_tab[i2];
+                    i2++;
+                    i++;
+                }
+            }
+        }
+
+      
+    }
+}
---
+++ AlgorithmTest/TriFusionTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithm;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace AlgorithmTest
+{
+    [TestFixture( )]
+    public class TriFusionTest
+    {
+        private TriFusion _triFusion;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _triFusion=new TriFusion();
+        }
+
+        [Test]
+        public void Should_Merge_Table()
+        {
+            var table = new int[] {0,3, 4, 1,2};
+            var expected = new int[] {0,1,2,3, 4};
+            var begin = 0;
+            var middle = 2;
+            var end = 4;
+
+            _triFusion.fusionner(table, begin, middle, end);
+
+            Assert.AreEqual(expected, table);
+            
+;        }
+
+        [Test]
+        public void Should_Merge_Table2()
+        {
+            var tab = new int[] { 0, 3, 4, 1, 2 ,5};
+            var expected = new int[] { 0, 1, 2, 3, 4,5 };
+            var begin = 0;
+            var middle = 2;
+            var end = 5;
+
+            _triFusion.fusionner(tab, begin, middle, end);
+
+            Assert.AreEqual(expected, tab);
+        }
+
+        [Test]
+        public void Should_Tri_Table()
+        {
+            var tab = new int[] {4, 3, 2};
+            var begin = 0;
+            var expected = new int [] {2, 3, 4};
+            
+            _triFusion.triFusion(tab,begin,tab.Length-1);
+
+            Assert.AreEqual(expected,tab);
+        }
+    }
+}
